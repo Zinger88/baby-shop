7e8ac55d055ce7789a2eeeacc31bb61097dc91d8
Zinger88 <kylinar88@gmail.com>
2024-09-06 17:12:29 +0300

    main: remove unusable files from project - update gitignore - add test ssr route (about)

---
+++ src/app/about/components/Currency.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export const Currency = () => {
+    return (
+        <div>
+            <h1>Currency</h1>
+        </div>
+    );
+};
---
+++ src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import { Currency } from "./components/Currency";
+
+const About = async () => {
+    const data: Response  = await fetch('https://api.vercel.app/blog');
+    const posts: Record<string, unknown>[] = await data.json();
+
+    return (
+        <div>
+            <h1>About Us</h1>
+            <ul>
+                {posts.map((post: Record<string, any>) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+            <Currency />
+        </div>
+    );
+};
+
+export default About;
