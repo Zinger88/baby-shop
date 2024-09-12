b98c8c61a7007b8662abb55d5db5010a26336373
‘Dima’ <sammerkarma@yandex.ru>
2024-09-12 19:06:38 +0300

    add color switcher

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,24 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {
-            colors: {
-                primary: '#f6f4ea',
-                //primary: '#f4f0db',
-                background: 'var(--background)',
-                foreground: 'var(--foreground)',
-            },
-        },
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      width: {
+        '16.1': '4.1rem',
+      }
     },
-    plugins: [],
+  },
+  plugins: [],
 };
+
 export default config;
