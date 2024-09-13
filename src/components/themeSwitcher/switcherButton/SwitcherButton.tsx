815606ddccfafee7cad7e312be6d36933f804128
Zinger88 <kylinar88@gmail.com>
2024-09-13 18:23:27 +0300

    fix/toogle-theme: remove sync with LS. refactoring for component - add icons instead of images

--- src/components/themeSwitcher/switcherButton/SwitcherButton.tsx
+++ src/components/themeSwitcher/switcherButton/SwitcherButton.tsx
@@ -1,33 +1,24 @@
-import Image from 'next/image';
+import {
+    MoonIcon,
+    SunIcon,
+} from '@heroicons/react/24/outline';
 
 interface ButtonProps {
     onClick: () => void;
-    isChecked: boolean;
+    isDark: boolean;
 }
 
-export const SwitcherButton: React.FC<ButtonProps> = ({ onClick, isChecked }) => {
+export const SwitcherButton: React.FC<ButtonProps> = ({ onClick, isDark }) => {
     return (
         <button
             onClick={onClick}
             className="relative flex items-center justify-between w-16.1 h-8 bg-gray-200  rounded-full p-1 transition-colors duration-300"
         >
-            <Image
-                src={"/images/white-sun.svg"}
-                alt="White Sun"
-                width={50}
-                height={50}
-                className={`absolute left-0.5 w-7 h-7 transition-opacity duration-300 `}
-            />
-            <Image
-                src={"/images/black-sun.svg"}
-                alt="Black Sun"
-                width={50}
-                height={50}
-                className={`absolute right-0.5 w-7 h-7 transition-opacity duration-300 `}
-            />
+                <MoonIcon opacity={isDark ? 1 : 0} />
+                <SunIcon opacity={isDark ? 0 : 1}/>
 
             <div
-                className={`absolute w-7 h-7 rounded-full shadow-md transform transition-transform duration-300 ${isChecked ? 'translate-x-8 bg-gray-800' : 'translate-x-0 bg-white'}`}
+                className={`absolute w-7 h-7 rounded-full shadow-md transform transition-transform duration-300 ${isDark ? 'translate-x-8 bg-gray-800' : 'translate-x-0 bg-white'}`}
             />
         </button>
     );
